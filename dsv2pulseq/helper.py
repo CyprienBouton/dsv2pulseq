4257cd6cf3d8254a0e1bc996634c4587217d3775
CyprienBouton <boutoncyprien@yahoo.com>
2025-02-06 16:02:07 +0100

    waveform_from_seqblock in dsv2pulseq/helper.py to work with pypulseq==1.4.2

--- dsv2pulseq/helper.py
+++ dsv2pulseq/helper.py
@@ -6,23 +6,21 @@ import numpy as np
 from pypulseq.Sequence.sequence import Sequence
 from pypulseq.make_delay import make_delay
 
-def waveform_from_seqblock(seq_block):
+def waveform_from_seqblock(seq_block, system):
     """
-    extracts gradient waveform from Pypulseq sequence block
+    extracts gradient waveform from Pypulseq sequence block.
+    Generate a sequence of magnitudes sampled at every gradient raster time.
+    
     """
-
-    if seq_block.channel == 'x':
-        axis = 0
-    elif seq_block.channel == 'y':
-        axis = 1
-    elif seq_block.channel == 'z':
-        axis = 2
-    else:
-        raise ValueError('No valid gradient waveform')
+    assert seq_block.channel in ['x', 'y', 'z'], 'No valid gradient waveform'
     dummy_seq = Sequence() # helper dummy sequence
     dummy_seq.add_block(make_delay(d=1e-3)) # dummy delay to allow gradients starting at a nonzero value
     dummy_seq.add_block(seq_block)
-    return dummy_seq.gradient_waveforms()[axis,100:-1] # last value is a zero that does not belong to the waveform
+    grad = 'g'+seq_block.channel
+    grad_amp = dummy_seq.waveforms_export()[grad]*1e3 # units in kHz
+    t = dummy_seq.waveforms_export()['t_'+grad]
+    t_sampled = np.arange(t[0], t[-1]+system.grad_raster_time, system.grad_raster_time)
+    return np.interp(t_sampled, t, grad_amp)
 
 def round_up_to_raster(number, decimals=0):
     """
