6819bc4acae915b4835b8c1d36c796156cffc5e8
CyprienBouton <boutoncyprien@yahoo.com>
2025-02-13 16:20:20 +0100

    up waveform_from_seqblock in helpers.py fix for waveform

--- dsv2pulseq/helper.py
+++ dsv2pulseq/helper.py
@@ -13,12 +13,16 @@ def waveform_from_seqblock(seq_block, system):
     
     """
     assert seq_block.channel in ['x', 'y', 'z'], 'No valid gradient waveform'
-    dummy_seq = Sequence() # helper dummy sequence
-    dummy_seq.add_block(make_delay(d=1e-3)) # dummy delay to allow gradients starting at a nonzero value
-    dummy_seq.add_block(seq_block)
-    grad = 'g'+seq_block.channel
-    grad_amp = dummy_seq.waveforms_export()[grad]*1e3 # units in kHz
-    t = dummy_seq.waveforms_export()['t_'+grad]
+    if hasattr(seq_block, 'waveform') and seq_block.waveform is not None:
+        grad_amp = seq_block.waveform
+        t = seq_block.tt
+    else:
+        dummy_seq = Sequence() # helper dummy sequence
+        dummy_seq.add_block(make_delay(d=1e-3)) # dummy delay to allow gradients starting at a nonzero value
+        dummy_seq.add_block(seq_block)
+        grad = 'g'+seq_block.channel
+        grad_amp = dummy_seq.waveforms_export()[grad]*1e3 # units in kHz
+        t = dummy_seq.waveforms_export()['t_'+grad]
     t_sampled = np.arange(t[0], t[-1]+system.grad_raster_time, system.grad_raster_time)
     return np.interp(t_sampled, t, grad_amp)
 
