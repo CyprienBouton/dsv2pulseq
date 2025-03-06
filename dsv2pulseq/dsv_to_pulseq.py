48ebd864ccd041b4b4f4c4a6e2dafde8352266ba
CyprienBouton <boutoncyprien@yahoo.com>
2025-03-06 19:43:33 +0100

    [UP] dsv2pulseq/dsv_to_pulseq.py to work with indep and elliptical RF Tx

--- dsv2pulseq/dsv_to_pulseq.py
+++ dsv2pulseq/dsv_to_pulseq.py
@@ -7,7 +7,9 @@ from dsv2pulseq.read_dsv import read_dsv
 defaults = {'out_file': 'external.seq',
             'ref_volt': 223.529007,
             'lead_time': 100,
-            'hold_time': 30}
+            'hold_time': 30,
+            'indep_Tx': False,
+            'elliptical_Tx': True}
 
 def main(args):
 
@@ -30,8 +32,14 @@ if __name__ == "__main__":
     parser.add_argument('-r', '--ref_volt', help='Reference voltage of simulation [V].', type=float)
     parser.add_argument('-b', '--lead_time', help='RF lead time [us] (minimum time between start of event block and beginning of RF).', type=int)
     parser.add_argument('-a', '--hold_time', help='RF hold time [us] (minimum time from end of RF to end of event block).', type=int)
+    parser.add_argument('--indep_Tx',  action=argparse.BooleanOptionalAction, help="If there is two independents RF transmitter")
+    parser.add_argument('--elliptical_Tx',  action=argparse.BooleanOptionalAction, help="If the RF transmitter is elliptical")
     
     parser.set_defaults(**defaults)
     args = parser.parse_args()
 
+    if args.indep_Tx:
+        # 1/sqrt(2) if circular siemens values for RF elliptical Tx
+        factor = 0.4457 if args.elliptical_Tx else 1/(2**0.5)
+        args.ref_volt *= factor
     main(args)
